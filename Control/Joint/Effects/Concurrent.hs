9b7fc9fd8208a9cfaadfe3ee946d3aaf69456dc6
Murat Kasimov <iokasimov.m@gmail.com>
2020-02-01 10:31:28 +0300

    Define Composition instances for Concrurrent and Transactional

--- Control/Joint/Effects/Concurrent.hs
+++ Control/Joint/Effects/Concurrent.hs
@@ -1,6 +1,10 @@
 module Control.Joint.Effects.Concurrent where
 
-import "async" Control.Concurrent.Async (Concurrently)
-import "joint" Control.Joint.Abilities (Liftable)
+import "async" Control.Concurrent.Async (Concurrently, runConcurrently)
+import "joint" Control.Joint.Abilities (Composition (Primary, run), Liftable)
+
+instance Composition Concurrently where
+	type Primary Concurrently a = IO a
+	run = runConcurrently
 
 type Concurrent = Liftable Concurrently
--- Control/Joint/Effects/Transactional.hs
+++ Control/Joint/Effects/Transactional.hs
@@ -1,6 +1,10 @@
 module Control.Joint.Effects.Transactional where
 
-import "stm" Control.Monad.STM (STM)
-import "joint" Control.Joint.Abilities (Liftable)
+import "stm" Control.Monad.STM (STM, atomically)
+import "joint" Control.Joint.Abilities (Composition (Primary, run), Liftable)
+
+instance Composition STM where
+	type Primary STM a = IO a
+	run = atomically
 
 type Transactional = Liftable STM
