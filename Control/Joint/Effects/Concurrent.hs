9cb5ec2cb394ee26fd076f8215b92dcad40ec9ea
Murat Kasimov <iokasimov.m@gmail.com>
2020-02-01 10:55:14 +0300

    Define Adaptable instances for Concrurrent and Transactional

--- Control/Joint/Effects/Concurrent.hs
+++ Control/Joint/Effects/Concurrent.hs
@@ -1,10 +1,13 @@
 module Control.Joint.Effects.Concurrent where
 
 import "async" Control.Concurrent.Async (Concurrently, runConcurrently)
-import "joint" Control.Joint.Abilities (Composition (Primary, run), Liftable)
+import "joint" Control.Joint.Abilities (Composition (Primary, run), Adaptable (adapt), Liftable)
 
 instance Composition Concurrently where
 	type Primary Concurrently a = IO a
 	run = runConcurrently
+
+instance Adaptable STM IO where
+	adapt = atomically
 
 type Concurrent = Liftable Concurrently
--- Control/Joint/Effects/Transactional.hs
+++ Control/Joint/Effects/Transactional.hs
@@ -1,10 +1,13 @@
 module Control.Joint.Effects.Transactional where
 
 import "stm" Control.Monad.STM (STM, atomically)
-import "joint" Control.Joint.Abilities (Composition (Primary, run), Liftable)
+import "joint" Control.Joint.Abilities (Composition (Primary, run), Adaptable (adapt), Liftable)
 
 instance Composition STM where
 	type Primary STM a = IO a
 	run = atomically
+
+instance Adaptable STM IO where
+	adapt = atomically
 
 type Transactional = Liftable STM
